09db4ec471d11a73d2c1b8c46c874b880fa10e68
slashmach1 <slashmach1@gmail.com>
2024-07-09 10:52:36 -0500

    flathub repo

--- build.sh
+++ build.sh
@@ -4,14 +4,15 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
-
+### Set flatpak
+mkdir -p /usr/etc/flatpak/remotes.d
+curl -Lo /usr/etc/flatpak/remotes.d/flathub.flatpakrepo https://dl.flathub.org/repo/flathub.flatpakrepo
 ### Install packages
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 rpm-ostree override remove fedora-workstation-repositories firefox firefox-langpacks  
 rpm-ostree install distrobox openresolv iwd steam-devices
 rpm-ostree install /tmp/akmod-hp-wmi-0-0.10.x86_64.rpm /tmp/hp-wmi-0-0.10.x86_64.rpm /tmp/kmod-hp-wmi-0-0.10.x86_64.rpm
-echo "AutomaticUpdatePolicy=stage" | sudo tee --append /etc/rpm-ostreed.conf
-cp -r /tmp/NetworkManager/. /etc/NetworkManager/
-cp -r /tmp/modprobe.d/. /etc/modprobe.d/
+echo "AutomaticUpdatePolicy=stage" | sudo tee --append /usr/etc/rpm-ostreed.conf
+cp -r /tmp/NetworkManager/. /usr/etc/NetworkManager/
+cp -r /tmp/modprobe.d/. /usr/etc/modprobe.d/
 systemctl enable iwd.service
 systemctl disable wpa_supplicant.service
