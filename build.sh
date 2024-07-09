e92a17b69379e4a0b51b36648cef050587516950
slashmach1 <slashmach1@gmail.com>
2024-07-09 11:43:16 -0500

    chrome-fix-4

--- build.sh
+++ build.sh
@@ -4,15 +4,15 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
-### google chrome workaround
-touch /etc/default/google-chrome
+### google chrome
+sed -i '0,/enabled=0/s//enabled=1/'  /etc/yum.repos.d/google-chrome.repo
 ### Set flatpak
 mkdir -p /usr/etc/flatpak/remotes.d
 curl -Lo /usr/etc/flatpak/remotes.d/flathub.flatpakrepo https://dl.flathub.org/repo/flathub.flatpakrepo
 ### Install packages
 rpm-ostree override remove fedora-workstation-repositories firefox firefox-langpacks  
-rpm-ostree install distrobox openresolv iwd steam-devices
-rpm-ostree install /tmp/akmod-hp-wmi-0-0.10.x86_64.rpm /tmp/hp-wmi-0-0.10.x86_64.rpm /tmp/kmod-hp-wmi-0-0.10.x86_64.rpm https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+rpm-ostree install distrobox openresolv iwd steam-devices google-chrome-stable
+rpm-ostree install /tmp/akmod-hp-wmi-0-0.10.x86_64.rpm /tmp/hp-wmi-0-0.10.x86_64.rpm /tmp/kmod-hp-wmi-0-0.10.x86_64.rpm
 echo "AutomaticUpdatePolicy=stage" | sudo tee --append /etc/rpm-ostreed.conf
 cp -r /tmp/NetworkManager/. /etc/NetworkManager/
 cp -r /tmp/modprobe.d/. /etc/modprobe.d/
