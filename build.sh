354ca2df3cf77ce3356f65ca2ebac41205448e1b
slashmach1 <slashmach1@gmail.com>
2024-07-09 12:15:01 -0500

    chrome-fix-6

--- build.sh
+++ build.sh
@@ -5,13 +5,14 @@ set -ouex pipefail
 RELEASE="$(rpm -E %fedora)"
 
 ### google chrome
+cp /tmp/google-chrome.repo /etc/yum.repos.d/google-chrome.repo
 sed -i '0,/enabled=0/s//enabled=1/'  /etc/yum.repos.d/google-chrome.repo
 ### Set flatpak
 mkdir -p /usr/etc/flatpak/remotes.d
 curl -Lo /usr/etc/flatpak/remotes.d/flathub.flatpakrepo https://dl.flathub.org/repo/flathub.flatpakrepo
 ### Install packages
 rpm-ostree override remove fedora-workstation-repositories firefox firefox-langpacks  
-rpm-ostree install distrobox openresolv iwd steam-devices google-chrome
+rpm-ostree install distrobox openresolv iwd steam-devices google-chrome-stable
 rpm-ostree install /tmp/akmod-hp-wmi-0-0.10.x86_64.rpm /tmp/hp-wmi-0-0.10.x86_64.rpm /tmp/kmod-hp-wmi-0-0.10.x86_64.rpm
 echo "AutomaticUpdatePolicy=stage" | sudo tee --append /etc/rpm-ostreed.conf
 cp -r /tmp/NetworkManager/. /etc/NetworkManager/
