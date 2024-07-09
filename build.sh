51873c4209619f29d16f20ff0de38e18a5f01385
slashmach1 <slashmach1@gmail.com>
2024-07-09 09:35:26 -0500

    wifi switch to iwd

--- build.sh
+++ build.sh
@@ -6,10 +6,11 @@ RELEASE="$(rpm -E %fedora)"
 
 
 ### Install packages
-rpm-ostree override remove fedora-workstation-repositories wpa_supplicant --install iwd                           
-rpm-ostree install distrobox openresolv
+rpm-ostree override remove fedora-workstation-repositories                          
+rpm-ostree install distrobox openresolv iwd
 rpm-ostree install /tmp/akmod-hp-wmi-0-0.10.x86_64.rpm /tmp/hp-wmi-0-0.10.x86_64.rpm /tmp/kmod-hp-wmi-0-0.10.x86_64.rpm
 echo "AutomaticUpdatePolicy=stage" | sudo tee --append /etc/rpm-ostreed.conf
 cp -r /tmp/NetworkManager/. /etc/NetworkManager/
 cp -r /tmp/modprobe.d/. /etc/modprobe.d/
-systemctl enable iwd
+systemctl enable iwd.service
+systemctl disable wpa_supplicant.service
