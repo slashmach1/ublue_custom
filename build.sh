2418160d96d3adc3a1c67177577b0a25f7ce2ac8
slashmach1 <76792323+slashmach1@users.noreply.github.com>
2024-07-09 06:05:24 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -20,32 +20,13 @@ flatpak remote-modify --enable flathub
 flatpak install flathub \
     org.mozilla.firefox \
     org.freedesktop.Platform.ffmpeg-full  `# for firefox hardware decoding` \
-    com.github.tchx84.Flatseal \
-    com.bitwarden.desktop \
-    com.github.qarmin.czkawka 
+    com.github.tchx84.Flatseal 
     
 #overrides
 rpm-ostree override remove firefox firefox-langpacks fedora-workstation-repositories
-rpm-ostree override remove mesa-va-drivers \
-    --install intel-media-driver
-
-mkdir -p /etc/rpm-ostree/origin.d
-
-tee /etc/rpm-ostree/origin.d/intel-media-driver.yaml <<EOF
-packages:
-  - intel-media-driver
-override-remove:
-  - mesa-va-drivers
-EOF
-
-rpm-ostree ex rebuild
 
 # this installs a package from fedora repos
 rpm-ostree install distrobox
-rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-
-#kargs
-rpm-ostree kargs --append=i915.enable_guc=2
 
 #config
 echo "AutomaticUpdatePolicy=stage" | sudo tee --append /etc/rpm-ostreed.conf
