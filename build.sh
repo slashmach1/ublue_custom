6d2adc9bb736893e9230e33a45f2b11ee006ae51
slashmach1 <76792323+slashmach1@users.noreply.github.com>
2024-07-09 07:19:18 -0500

    Update build.sh
    Flatpak config remove and chrome install

--- build.sh
+++ build.sh
@@ -6,18 +6,12 @@ RELEASE="$(rpm -E %fedora)"
 
 
 ### Install packages
-flatpak uninstall --all --delete-data --assumeyes \
-&& flatpak remote-modify --disable fedora \
-&& flatpak remote-delete --system flathub  \
-&& flatpak remote-add --system --if-not-exists \
-&& flatpak remote-modify --enable flathub \
-&& flatpak install flathub \
-    org.mozilla.firefox \
-    org.freedesktop.Platform.ffmpeg-full \
-    com.github.tchx84.Flatseal 
 rpm-ostree override remove \
     firefox \
     firefox-langpacks \
     fedora-workstation-repositories
 rpm-ostree install distrobox 
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+rpm-ostree install google-chrome-stable_current_x86_64.rpm
+rm google-chrome-stable_current_x86_64.rpm -f
 echo "AutomaticUpdatePolicy=stage" | sudo tee --append /etc/rpm-ostreed.conf
