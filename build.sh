36d89de806d9baeb0ba6c4d6da235b3452b854e9
slashmach1 <76792323+slashmach1@users.noreply.github.com>
2024-07-09 08:37:33 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,56 +6,33 @@ RELEASE="$(rpm -E %fedora)"
 
 
 ### Install packages
-rpm-ostree override remove \
+rpm-ostree override remove 
     firefox \
     firefox-langpacks \
     fedora-workstation-repositories
     
 rpm-ostree override remove libavcodec-free \
-                           libavfilter-free \
-                           libavformat-free \
-                           libavutil-free \
-                           libpostproc-free \ 
-                           libswresample-free \ 
-                           libswscale-free \
-                           --install ffmpeg
+libavfilter-free \
+libavformat-free \
+libavutil-free \
+libpostproc-free \ 
+libswresample-free \ 
+libswscale-free \
+--install ffmpeg
                            
 rpm-ostree override remove wpa_supplicant \
-                           --install iwd
+--install iwd
                            
 rpm-ostree install distrobox \
-                   gstreamer1-plugin-libav \
-                   gstreamer1-plugins-bad-free-extras \
-                   gstreamer1-plugins-bad-freeworld \
-                   gstreamer1-plugins-ugly \
-                   gstreamer1-vaapi \
-                   intel-media-driver \
-                   libva-intel-driver \
-                   libva-nvidia-driver \
-                   openresolv
-                   
-cat <<EOF | tee /etc/NetworkManager/conf.d/wifi_backend.conf
-[device]
-wifi.backend=iwd
-EOF
-
-cat <<EOF | tee /etc/NetworkManager/conf.d/dns.conf
-[main]
-dns=dnsmasq
-EOF
-
-cat <<EOF | tee /etc/NetworkManager/dnsmasq.d/ipv6-listen.conf
-listen-address=::1
-EOF
-
-cat <<EOF | tee /etc/NetworkManager/dnsmasq.d/cache.conf
-cache-size=1000
-EOF
-
-cat <<-EOF | tee /etc/NetworkManager/conf.d/rc-manager.conf
-[main]
-rc-manager=resolvconf
-EOF
+gstreamer1-plugin-libav \
+gstreamer1-plugins-bad-free-extras \
+gstreamer1-plugins-bad-freeworld \
+gstreamer1-plugins-ugly \
+gstreamer1-vaapi \
+intel-media-driver \
+libva-intel-driver \
+libva-nvidia-driver \
+openresolv
 
 rpm-ostree install https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
 rpm-ostree install /tmp/akmod-hp-wmi-0-0.10.x86_64.rpm /tmp/hp-wmi-0-0.10.x86_64.rpm /tmp/kmod-hp-wmi-0-0.10.x86_64.rpm
