7aedc9ac9f90e0cf12d4063737efb51c0e16dd36
slashmach1 <76792323+slashmach1@users.noreply.github.com>
2024-07-09 09:07:05 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ RELEASE="$(rpm -E %fedora)"
 
 
 ### Install packages
-rpm-ostree override remove firefox firefox-langpacks fedora-workstation-repositories libavcodec-free libavfilter-free libavformat-free libavutil-free libpostproc-free libswresample-free libswscale-free wpa_supplicant --install ffmpeg --install iwd                           
-rpm-ostree install distrobox gstreamer1-plugin-libav gstreamer1-plugins-bad-free-extras gstreamer1-plugins-bad-freeworld gstreamer1-plugins-ugly gstreamer1-vaapi intel-media-driver libva-intel-driver libva-nvidia-driver openresolv
+rpm-ostree override remove firefox firefox-langpacks fedora-workstation-repositories wpa_supplicant--install iwd                           
+rpm-ostree install ffmpeg distrobox gstreamer1-plugin-libav gstreamer1-plugins-bad-free-extras gstreamer1-plugins-bad-freeworld gstreamer1-plugins-ugly gstreamer1-vaapi intel-media-driver libva-intel-driver libva-nvidia-driver openresolv
 rpm-ostree install https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
 rpm-ostree install /tmp/akmod-hp-wmi-0-0.10.x86_64.rpm /tmp/hp-wmi-0-0.10.x86_64.rpm /tmp/kmod-hp-wmi-0-0.10.x86_64.rpm
 echo "AutomaticUpdatePolicy=stage" | sudo tee --append /etc/rpm-ostreed.conf
