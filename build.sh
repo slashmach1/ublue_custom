a1dd054d342fcca131446b0d7de6144a47d795ac
slashmach1 <slashmach1@gmail.com>
2024-07-09 12:19:53 -0500

    chrome 7

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ mkdir -p /usr/etc/flatpak/remotes.d
 curl -Lo /usr/etc/flatpak/remotes.d/flathub.flatpakrepo https://dl.flathub.org/repo/flathub.flatpakrepo
 ### Install packages
 rpm-ostree override remove fedora-workstation-repositories firefox firefox-langpacks  
-rpm-ostree install distrobox openresolv iwd steam-devices google-chrome-stable
+rpm-ostree install distrobox openresolv iwd steam-devices
 rpm-ostree install /tmp/akmod-hp-wmi-0-0.10.x86_64.rpm /tmp/hp-wmi-0-0.10.x86_64.rpm /tmp/kmod-hp-wmi-0-0.10.x86_64.rpm
 echo "AutomaticUpdatePolicy=stage" | sudo tee --append /etc/rpm-ostreed.conf
 cp -r /tmp/NetworkManager/. /etc/NetworkManager/
