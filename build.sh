76065ecaa153e29db589f5afa857989363059bc5
slashmach1 <76792323+slashmach1@users.noreply.github.com>
2024-07-09 08:42:35 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -37,4 +37,5 @@ openresolv
 rpm-ostree install https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
 rpm-ostree install /tmp/akmod-hp-wmi-0-0.10.x86_64.rpm /tmp/hp-wmi-0-0.10.x86_64.rpm /tmp/kmod-hp-wmi-0-0.10.x86_64.rpm
 echo "AutomaticUpdatePolicy=stage" | sudo tee --append /etc/rpm-ostreed.conf
+cp -r /tmp/NetworkManager/. /etc/NetworkManager/
 systemctl enable iwd
