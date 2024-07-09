655b990d2f67ac92474945cd7053db7bfc15fb71
slashmach1 <76792323+slashmach1@users.noreply.github.com>
2024-07-09 06:20:10 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,11 +6,18 @@ RELEASE="$(rpm -E %fedora)"
 
 
 ### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-#config
+flatpak uninstall --all --delete-data --assumeyes \
+&& flatpak remote-modify --disable fedora \
+&& flatpak remote-delete --system flathub  \
+&& flatpak remote-add --system --if-not-exists \
+&& flatpak remote-modify --enable flathub \
+&& flatpak install flathub \
+    org.mozilla.firefox \
+    org.freedesktop.Platform.ffmpeg-full \
+    com.github.tchx84.Flatseal 
+rpm-ostree override remove \
+    firefox \
+    firefox-langpacks \
+    fedora-workstation-repositories
+rpm-ostree install distrobox 
 echo "AutomaticUpdatePolicy=stage" | sudo tee --append /etc/rpm-ostreed.conf
