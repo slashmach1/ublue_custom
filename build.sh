a140ff6b699f96f28d316cb78d7a32952f0300fa
slashmach1 <slashmach1@gmail.com>
2024-07-09 15:44:20 -0500

    fix-2

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ set -ouex pipefail
 RELEASE="$(rpm -E %fedora)"
 
 ### Set flatpak
+mkdir -p /root/.cache/dconf
 flatpak remote-add --if-not-exists --system flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install --noninteractive --system flathub \
     app/com.github.tchx84.Flatseal/x86_64/stable \
@@ -13,22 +14,6 @@ flatpak install --noninteractive --system flathub \
     com.bitwarden.desktop \
     com.plexamp.Plexamp
 ### Install packages
-# Setup repo
-cat << EOF > /etc/yum.repos.d/google-chrome.repo
-[google-chrome]
-name=google-chrome
-baseurl=https://dl.google.com/linux/chrome/rpm/stable/x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-google
-EOF
-
-# Import signing key
-curl --retry 3 --retry-delay 2 --retry-all-errors -sL \
-  -o /etc/pki/rpm-gpg/RPM-GPG-KEY-google \
-  https://dl.google.com/linux/linux_signing_key.pub
-rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-google
 rpm-ostree install ffmpeg gstreamer1-plugin-libav gstreamer1-plugins-bad-free-extras gstreamer1-plugins-bad-freeworld gstreamer1-plugins-ugly gstreamer1-vaapi 
 rpm-ostree override remove fedora-workstation-repositories
 rpm-ostree install distrobox openresolv iwd steam-devices steam libva-nvidia-driver intel-media-driver libva-intel-driver
