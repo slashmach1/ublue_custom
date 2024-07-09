457736b7047b10769aba3110ca1175bacb24742c
slashmach1 <76792323+slashmach1@users.noreply.github.com>
2024-07-09 09:13:07 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,10 +6,11 @@ RELEASE="$(rpm -E %fedora)"
 
 
 ### Install packages
-rpm-ostree override remove firefox firefox-langpacks fedora-workstation-repositories wpa_supplicant--install iwd                           
-rpm-ostree install distrobox openresolv
+rpm-ostree override remove firefox firefox-langpacks fedora-workstation-repositories                         
+rpm-ostree install distrobox openresolv iwd
 rpm-ostree install https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
 rpm-ostree install /tmp/akmod-hp-wmi-0-0.10.x86_64.rpm /tmp/hp-wmi-0-0.10.x86_64.rpm /tmp/kmod-hp-wmi-0-0.10.x86_64.rpm
 echo "AutomaticUpdatePolicy=stage" | sudo tee --append /etc/rpm-ostreed.conf
 cp -r /tmp/NetworkManager/. /etc/NetworkManager/
-systemctl enable iwd
+systemctl disable wpa_supplicant.service
+systemctl enable iwd.service
