d42aa17521c75a040f8afbcfc1063c2a399f9654
slashmach1 <slashmach1@gmail.com>
2024-07-09 14:08:15 -0500

    flatpak fix

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ RELEASE="$(rpm -E %fedora)"
 ### Set flatpak
 flatpak remote-add --if-not-exists --system flathub https://flathub.org/repo/flathub.flatpakrepo
 #flatpak uninstall --system --noninteractive `flatpak list --all --columns origin,application|grep ^fedora|awk '{print $2}'|xargs`
-flatpak remote-delete --system fedora --force
+#flatpak remote-delete --system fedora --force
 flatpak install --noninteractive --system flathub \
     app/com.github.tchx84.Flatseal/x86_64/stable \
     app/io.github.dvlv.boxbuddyrs/x86_64/stable \
