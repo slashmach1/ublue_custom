b242ce40ff459d72928841998f6479c5f7473311
slashmach1 <slashmach1@gmail.com>
2024-07-09 20:02:28 -0500

    redirect /etc to /usr/lib

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ RELEASE="$(rpm -E %fedora)"
 rpm-ostree override remove fedora-workstation-repositories firefox firefox-langpacks
 rpm-ostree install distrobox openresolv iwd steam-devices
 rpm-ostree install --idempotent /tmp/akmod-hp-wmi-0-0.10.x86_64.rpm /tmp/hp-wmi-0-0.10.x86_64.rpm /tmp/kmod-hp-wmi-0-0.10.x86_64.rpm
-cp -r /tmp/NetworkManager/. /etc/NetworkManager/
-cp -r /tmp/modprobe.d/. /etc/modprobe.d/
-cp -r /tmp/lib/sysctl.d/. /etc/sysctl.d/
+cp -r /tmp/NetworkManager/. /usr/lib/NetworkManager/
+cp -r /tmp/modprobe.d/. /usr/lib/modprobe.d/
+cp -r /tmp/lib/sysctl.d/. /usr/lib/sysctl.d/
 systemctl enable iwd.service
 systemctl disable wpa_supplicant.service
