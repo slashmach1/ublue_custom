eaccc0f0ad7519a0133f38e2491f26e11ec9cd9b
slashmach1 <76792323+slashmach1@users.noreply.github.com>
2024-07-11 09:55:50 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ rpm-ostree override remove fedora-workstation-repositories firefox firefox-langp
 rpm-ostree override remove toolbox --install distrobox
 rpm-ostree install openresolv iwd
 rpm-ostree install --idempotent /tmp/akmod-hp-wmi-0-0.10.x86_64.rpm /tmp/hp-wmi-0-0.10.x86_64.rpm /tmp/kmod-hp-wmi-0-0.10.x86_64.rpm
+rpm-ostree install ffmpeg gstreamer1-plugin-libav gstreamer1-plugins-bad-free-extras gstreamer1-plugins-bad-freeworld gstreamer1-plugins-ugly gstreamer1-vaapi libva-nvidia-driver intel-media-driver libva-intel-driver
 cp -r /tmp/NetworkManager/. /usr/lib/NetworkManager/
 cp -r /tmp/modprobe.d/. /usr/lib/modprobe.d/
 cp -r /tmp/lib/sysctl.d/. /usr/lib/sysctl.d/
