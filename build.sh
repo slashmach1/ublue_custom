68d3e12ec802807c75a9a27d37a5684dc924b26a
slashmach1 <76792323+slashmach1@users.noreply.github.com>
2024-07-11 11:51:21 -0500

    Update build.sh
    services enable

--- build.sh
+++ build.sh
@@ -12,6 +12,13 @@ rpm-ostree install --idempotent /tmp/akmod-hp-wmi-0-0.10.x86_64.rpm /tmp/hp-wmi-
 cp -r /tmp/NetworkManager/. /usr/lib/NetworkManager/
 cp -r /tmp/modprobe.d/. /usr/lib/modprobe.d/
 cp -r /tmp/lib/sysctl.d/. /usr/lib/sysctl.d/
+cp -r /tmp/lib/systemd/. /usr/lib/systemd/
 cp /tmp/rpm-ostreed.conf /etc/rpm-ostreed.conf
+chmod go-w /usr/lib/systemd/system/*.*
+systemctl enable dconf-update.service
+systemctl enable flatpak-add-flathub-repo.service
+systemctl enable flatpak-replace-fedora-apps.service
+systemctl enable flatpak-cleanup.timer
+systemctl enable rpm-ostreed-automatic.timer
 systemctl enable iwd.service
 systemctl disable wpa_supplicant.service
