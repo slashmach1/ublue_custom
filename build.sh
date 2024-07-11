b3f72cc1b9fa71a61b94c8fcd9364a39c50eca5a
slashmach1 <76792323+slashmach1@users.noreply.github.com>
2024-07-11 08:44:04 -0500

    Update build.sh
    steam, gamescope, and lutris.

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ RELEASE="$(rpm -E %fedora)"
 ### Install packages
 rpm-ostree override remove fedora-workstation-repositories firefox firefox-langpacks
 rpm-ostree override remove toolbox --install distrobox
-rpm-ostree install openresolv iwd
+rpm-ostree install openresolv iwd gamescope steam steam-devices lutris
 rpm-ostree install --idempotent /tmp/akmod-hp-wmi-0-0.10.x86_64.rpm /tmp/hp-wmi-0-0.10.x86_64.rpm /tmp/kmod-hp-wmi-0-0.10.x86_64.rpm
 cp -r /tmp/NetworkManager/. /usr/lib/NetworkManager/
 cp -r /tmp/modprobe.d/. /usr/lib/modprobe.d/
