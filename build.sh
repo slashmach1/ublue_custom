aed6cadac76cb92798ae132f2a3a9599414e8de9
slashmach1 <76792323+slashmach1@users.noreply.github.com>
2024-07-13 10:06:22 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ RELEASE="$(rpm -E %fedora)"
 rpm-ostree override remove fedora-workstation-repositories firefox firefox-langpacks
 rpm-ostree override remove toolbox --install distrobox
 rpm-ostree install openresolv iwd
-rpm-ostree install --idempotent /tmp/hp-wmi-0-0.10.x86_64.rpm /tmp/kmod-hp-wmi-0-0.10.x86_64.rpm /tmp/*xone*.rpm
-#/tmp/akmod-hp-wmi-0-0.10.x86_64.rpm /tmp/hp-wmi-0-0.10.x86_64.rpm #/tmp/kmod-hp-wmi-0-0.10.x86_64.rpm #/tmp/*xone*.rpm
+rpm-ostree install --idempotent /tmp/hp-wmi-0-0.10.x86_64.rpm /tmp/kmod-hp-wmi-0-0.10.x86_64.rpm /tmp/*xone*.rpm /tmp/akmod-hp-wmi-0-0.10.x86_64.rpm 
+#/tmp/hp-wmi-0-0.10.x86_64.rpm #/tmp/kmod-hp-wmi-0-0.10.x86_64.rpm #/tmp/*xone*.rpm
 cp -r /tmp/NetworkManager/. /usr/lib/NetworkManager/
 cp -r /tmp/modprobe.d/. /usr/lib/modprobe.d/
 cp -r /tmp/lib/sysctl.d/. /usr/lib/sysctl.d/
