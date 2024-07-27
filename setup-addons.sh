7ea1f373154ee546969e38c975c9fe81ae762bc5
slashmach1 <slashmach1@gmail.com>
2024-07-27 10:25:39 -0500

    disable xpad

--- setup-addons.sh
+++ setup-addons.sh
@@ -7,7 +7,7 @@ RELEASE="$(rpm -E %fedora)"
 curl https://pkgs.tailscale.com/stable/fedora/tailscale.repo -o /etc/yum.repos.d/tailscale.repo
 wget https://copr.fedorainfracloud.org/coprs/kylegospo/system76-scheduler/repo/fedora-$(rpm -E %fedora)/kylegospo-system76-scheduler-fedora-$(rpm -E %fedora).repo -O /etc/yum.repos.d/_copr_kylegospo-system76-scheduler.repo
 rpm-ostree install system76-scheduler tailscale ptyxis
-rpm-ostree install --idempotent /tmp/*xpadneo*.rpm
+#rpm-ostree install --idempotent /tmp/*xpadneo*.rpm
 rm /etc/yum.repos.d/tailscale.repo -f
 rm /etc/yum.repos.d/_copr_kylegospo-system76-scheduler.repo -f
 systemctl enable --now com.system76.Scheduler.service
