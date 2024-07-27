920676dad18a25fd9030885e6ffae3153f750879
slashmach1 <slashmach1@gmail.com>
2024-07-27 09:56:33 -0500

    akmods test

--- setup-hp-wmi.sh
+++ setup-hp-wmi.sh
@@ -3,5 +3,7 @@
 set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
+sed -i 's/enabled=1/enabled=0/' /etc/yum.repos.d/*updates-*.repo
 rpm-ostree install akmods
-rpm-ostree install --idempotent /tmp/akmod-hp-wmi-0-0.10.x86_64.rpm  /tmp/hp-wmi-0-0.10.x86_64.rpm /tmp/kmod-hp-wmi-0-0.10.x86_64.rpm=
+rpm-ostree install --idempotent /tmp/akmod-hp-wmi-0-0.10.x86_64.rpm  /tmp/hp-wmi-0-0.10.x86_64.rpm /tmp/kmod-hp-wmi-0-0.10.x86_64.rpm
+sed -i 's/enabled=0/enabled=1/' /etc/yum.repos.d/*updates-*.repo
