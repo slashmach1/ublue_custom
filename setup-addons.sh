fb3661be0302a2b4f3e2af59932fc3ad7b2a90c1
slashmach1 <slashmach1@gmail.com>
2024-10-18 08:32:33 -0500

    fixes and cleanup

--- setup-addons.sh
+++ setup-addons.sh
@@ -5,5 +5,7 @@ set -ouex pipefail
 RELEASE="$(rpm -E %fedora)"
 ##setup repos
 curl https://pkgs.tailscale.com/stable/fedora/tailscale.repo -o /etc/yum.repos.d/tailscale.repo
-rpm-ostree install wireguard-tools tailscale distrobox openresolv iwd android-tools steam-devices /tmp/*.rpm
+sed -i 's/enabled=1/enabled=0/' /etc/yum.repos.d/*updates-*.repo
+rpm-ostree install --idempotent wireguard-tools tailscale distrobox openresolv iwd android-tools steam-devices /tmp/*.rpm
+sed -i 's/enabled=0/enabled=1/' /etc/yum.repos.d/*updates-*.repo
 rm /etc/yum.repos.d/tailscale.repo -f
